85faabe963f113998dfb90e595b4548a79634474
Sofia Campos <sfabilene@gmail.com>
2023-03-02 23:27:01 -0600

    add server

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const URL = "http://3.14.9.247:3333/api/";
+const URL = "https://iris-api-adue.onrender.com/api/";
 export const getHeartRate = async () => {
   const response = await fetch(URL + "pulse/user/63fbe07a465cbc082055261d");
   const data = await response.json();
